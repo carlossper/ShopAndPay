f65aa21460426f5d65e516497ef9a20a657bd8e1
AndreMachado94 <andremachado94@gmail.com>
2017-11-07 13:57:25 +0000

    Main Menu Added with buttons

--- MobileApp/app/src/main/java/com/feup/ei12078/shopandpay/BaseActivity.java
+++ MobileApp/app/src/main/java/com/feup/ei12078/shopandpay/BaseActivity.java
@@ -1,13 +1,10 @@
 package com.feup.ei12078.shopandpay;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -17,7 +14,7 @@ public class BaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_base);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +23,7 @@ public class BaseActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+*/
     }
 
     @Override
---
+++ MobileApp/app/src/main/java/com/feup/ei12078/shopandpay/SearchCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.feup.ei12078.shopandpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_category);
+    }
+}
